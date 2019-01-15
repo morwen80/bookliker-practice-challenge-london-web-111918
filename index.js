2191b01c3287d78c528683f349ce7c06aefdc56b
morwen80 <ribesnero80@gmail.com>
2019-01-15 16:31:01 +0000

    book list showing

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {});
+// document.addEventListener("DOMContentLoaded", fetchBooks());
+
+// const SHELF = []
+
+function fetchBooks(){
+  fetch("http://localhost:3000/books")
+  .then(response => response.json())
+  .then(data => {
+    // SHELF =  data
+    data.forEach(renderBook)
+  })
+
+const renderBook = (book) => {
+  const bookEl = document.createElement('li')
+    bookEl.innerHTML = `
+      <h3>${book.title}</h3>
+    `
+    document.querySelector("#list").appendChild(bookEl)
+}
+}
