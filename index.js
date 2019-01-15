936deb6b288b24a9a2466c9e16e33f25a8fb98fe
morwen80 <ribesnero80@gmail.com>
2019-01-15 19:22:58 +0000

    Book cover and description visible

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 // document.addEventListener("DOMContentLoaded", fetchBooks());
 
-// const SHELF = []
+function api(){
+  return fetch("http://localhost:3000/books").then(res => res.json())
+}
+
+const bookFansArray = []
 
 function fetchBooks(){
-  fetch("http://localhost:3000/books")
-  .then(response => response.json())
-  .then(data => {
-    // SHELF =  data
-    data.forEach(renderBook)
-  })
+  api().then(data => {data.forEach(bookList)
+})
 
-const renderBook = (book) => {
+const bookList = (book) => {
   const bookEl = document.createElement('li')
+    bookEl.setAttribute("onclick", `selectABook(${book.id})`);
     bookEl.innerHTML = `
-      <h3>${book.title}</h3>
+      <h3>${book.title}</h3> <input id="liked" type="checkbox"> Like this book?
     `
     document.querySelector("#list").appendChild(bookEl)
+  }
 }
+
+
+function selectABook(id) {
+  let bookShelf = document.createElement('div');
+  api().then(data => {
+  let book = data.find(book => book.id === id)
+    bookShelf.innerHTML = `
+      <img src="${book.img_url}">
+
+      <br>
+      ${book.description}
+      <hr>
+      <b>Users who liked this book: </b>${book.users}
+    `
+    document.querySelector("#show-panel").appendChild(bookShelf)
+})
 }
+
+
+fetchBooks()
